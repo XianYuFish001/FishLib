ebff1931554fd6a9df8ce5f61bbe9948ff045826
XianYuFish001 <xianyufish001@outlook.com>
2026-03-08 05:46:40 +0800

    我说怎么不补全, 原来是target没写对(

--- src/main/kotlin/com/fish/fishlib/network/AnnotationsPacket.kt
+++ src/main/kotlin/com/fish/fishlib/network/AnnotationsPacket.kt
@@ -3,5 +3,5 @@ package com.fish.fishlib.network
 @Target(AnnotationTarget.CLASS)
 annotation class FishNetworkPacket(val value: String)
 
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.FIELD)
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.FIELD)
 annotation class PacketStreamCodec
